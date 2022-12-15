e1d233a2580a6e33611869ab226e28a7bf18bf59
zr4nnk <axe14club.a1c@gmail.com>
2022-12-15 20:24:13 +0530

    New Commit
    First Update

---
+++ MyAbc.java
@@ -0,0 +1,8 @@
+public class Abc
+{
+    public static void main(String []a)
+    {
+        System.out.println("Hello This is first program.");
+        System.out.println("Using Github Projects of Desktop Version.");
+    }
+}
